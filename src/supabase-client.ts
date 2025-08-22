d9970d2ab140f9495478fc7888dcadbabe1d03bc
rohan <rohansharmas028@gmail.com>
2025-08-22 21:22:21 +0530

    first commit

---
+++ src/supabase-client.ts
@@ -0,0 +1,6 @@
+import {createClient} from '@supabase/supabase-js'
+
+export const supabase  = createClient(
+    "https://vtejkzxialqsdyuwhvgz.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0ZWprenhpYWxxc2R5dXdodmd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4NjUyMzYsImV4cCI6MjA3MTQ0MTIzNn0.6z91ADVRBk0RoxRvMrcqo92XE8uT9A1SyuXwQf0aaeI"
+)
